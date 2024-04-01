281776ab97f89995e1ef624453defd922406dcab
berna <78691060+bernaysl@users.noreply.github.com>
2024-04-01 15:20:55 +0300

    Update crypto_selenium.py
    hatalar giderildi, çalışıyor

--- crypto_selenium.py
+++ crypto_selenium.py
@@ -4,21 +4,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
-#chromeOptions = webdriver.ChromeOptions()
 options = webdriver.ChromeOptions()
-options.add_argument('--incognito')#gizli sekmede açması için
-options.add_argument('--headless') #sekme görünürde açılmasın biz bunu görmeyelim
+options.add_argument('--incognito')  # Gizli sekmede açması için
+options.add_argument('--headless')  # Sekme görünürde açılmasın
 
 driver = webdriver.Chrome(options=options)
-driver.maximize_window() #tam ekranda açılsın
-driver.delete_all_cookies() #çerezler gelmesin
+driver.maximize_window()  # Tam ekranda açılsın
+driver.delete_all_cookies()  # Çerezler gelmesin
 
-driver.get("https://tr.tradingview.com/chart/?symbol=FX_IDC%3AXAUTRYG") #bu linki aç
-driver.implicitly_wait(10) #site açılınca 10 saniye bekleme süresi ver, 10 saniyeden önce açılırsa direkt işleme 
-#site yüklenebilir ama fiyat bilgisi henüz yüklenmemiş olabilir o yüzden bekleme süresi koyuyorum
+driver.get("https://tr.tradingview.com/chart/?symbol=FX_IDC%3AXAUTRYG")  # Bu linki aç
+driver.implicitly_wait(10)  # Site açılınca 10 saniye bekleme süresi ver
 
-#sürekli değiştiği için linkin xpath'ini kopyaladım
+# Sürekli değiştiği için XPath'i kullanarak fiyat bilgisini al
+xpath = '/html/body/div[2]/div[6]/div/div[2]/div[1]/div[1]/div[2]/div[2]/div/div[2]/div[2]/span[1]/span[1]'
 while True:
-    fiyat_bilgisi=driver.find_element("xpath","/html/body/div[2]/div[6]/div/div[1]/div[1]/div[1]/div[2]/div[2]/div/div[2]/div[2]/span[1]/span[1]")
+    fiyat_bilgisi = driver.find_element(By.XPATH, xpath).text  # .text ile öğenin metin içeriğini al
     print(fiyat_bilgisi)
     sleep(3)
